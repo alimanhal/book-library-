618e0fb37a101d7d16343c9e6f58e63a868d9570
alimanhal <alimanhal@gmail.com>
2025-05-26 20:43:56 +0530

    Not Found

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import { BookProvider } from './BooksContext';
 import Books from './components/Books';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import BookDetails from './components/BookDetails';
+import NotFound from './components/NotFound';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 function App() {
   return (
     <BookProvider>
-    <Router>
-      <Routes>
-        <Route path='/' element={ <Books></Books> }></Route>
-        <Route path='/books/:bookId' element={<BookDetails></BookDetails>}></Route>
-      </Routes>
-    </Router>
-
-
-       
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path='/books' element={<Navigate to ="/"/>}></Route>
+          <Route path="/books/:bookId" element={<BookDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </BookProvider>
-   
+    
   );
 }
 
